ca0e3c7671ed94980047f9a7dc81a6fc44c8f714
lebrieh <89428782+lebrieh@users.noreply.github.com>
2022-06-01 17:40:40 -0500

    Add files via upload

---
+++ cannon.js
@@ -0,0 +1,23 @@
+class Cannon {
+
+    constructor (x, y, width, heigh, angle) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.heigh = heigh;
+        this.angle = angle;
+    }
+
+    display() {
+
+        //base
+
+
+
+        //cañon
+
+
+
+    }
+
+}
